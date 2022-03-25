7d49af2796222e4c57c2252d6c50dfdb30a2a4ef
Thomas Thirion <thirionthomas68100@gmail.com>
2022-03-25 13:40:56 +0100

    commit erreur

--- pages/_documents.js
+++ pages/_documents.js
@@ -8,9 +8,7 @@ class MyDocument extends Document{
                 <Head>
                 <link rel="stylesheet"
                  href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous"/>
-                <style>
-                    url @import('https://fonts.googleapis.com/css2? famille=Permanent+Marker&display=swap');
-                </style>
+                <link href="https://fonts.googleapis.com/css2?family=Hurricane&display=swap" rel="stylesheet"></link>
                 </Head>
                 <body>
                    <Main/>
