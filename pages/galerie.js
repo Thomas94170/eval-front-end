206775a7b919651dcec85768280bd941a7451359
Thomas Thirion <thirionthomas68100@gmail.com>
2022-03-28 11:06:47 +0200

    commit photo local

--- pages/galerie.js
+++ pages/galerie.js
@@ -5,12 +5,14 @@ import { Layout } from '../components/Layout'
 import { Footer } from '../components/Footer'
 
 export default function Galerie({res}) {
+  
   return (
     <>
       <Layout page='Galerie'>
+        {/* {JSON.stringify(res)} */}
       <br />
       <h1 className='text-center text-info border-bottom border-info'>Ma Galerie</h1>
-      {/* {JSON.stringify(res)} */}
+      
       <br />
       <br />
       <ul className="">
@@ -18,9 +20,9 @@ export default function Galerie({res}) {
            <li key={res} className='relative hover:shadow-md p-8
            border border-blue-300 rounded-3xl bg-blue-100 md:w-auto flex-1 mx-5'>
              <div>{res.name}</div>
-             <div>{res.img[0].formats.large.url}</div>
-             <div>{res.categories.name}</div>
-
+             {/* <div>{res.img[0].formats.large.url}</div> */}
+             <img src={`http://localhost:1337`+res.img[0].formats.thumbnail.url} className="" alt={res.name} width ="300px" height= "300px"/>
+             
              
            </li>
          ))}
